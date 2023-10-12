24b48459b44ab0961fc925c91a11557abebd9477
Demyrah Bivings <demyrahbivings@Demyrahs-iMac.local>
2023-10-12 19:07:56 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+//prompt ("Please confirm your order by typing the following: vanilla, vanilla, vanilla, strawberry, coffee, coffee")
+
+const flavor1 = Vanilla (localStorage.flavor1);
+console.log(localStorage.flavor1)
