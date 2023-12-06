6a59e888273d11876babd806d78196460f2099f0
Demyrah Bivings <demyrahbivings@Demyrahs-iMac.local>
2023-12-05 19:47:22 -0500

    updated code

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-prompt("Please confirm your order by typing the following: vanilla, vanilla, vanilla, strawberry, coffee, coffee")
-
+let userObject = {};
+let userInput = prompt("Please confirm your order by typing the following: vanilla, vanilla, vanilla, strawberry, coffee, coffee"); 
+userObject.data = userInput;
 
 const flavors = {
-    Vanilla: 3,
-    Strawberry: 1,
-    Coffee: 2,
+  vanilla: 3,
+  strawberry: 1,
+  coffee: 2,
 }
-console.table(flavors)
+console.table(userInput,flavors);
+console.log(Object.entries(flavors));
+
+for (const value in flavors) {
+  console.log(`You've ordered ${flavors[value]} flavors of ${value}`)
+}
+
+function printObj(obj) {
+  for (const variable in obj) {
+    console.log("the key is: " + variable +" " + "and the value is " + obj[variable])
+  }
+}
+
+printObj(flavors)
+
+const vanilla = document.getElementById("1");
+vanilla.textContent = flavors.vanilla
+
+const strawberry = document.getElementById("2");
+strawberry.textContent = flavors.strawberry
+
+const coffee = document.getElementById("3");
+coffee.textContent = flavors.coffee
+
+
